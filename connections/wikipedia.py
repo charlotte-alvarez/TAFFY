d9d1d5c96d74f8be5441996d413a898bb355e8ca
charlotte-alvarez <email@armandalvarez.com>
2026-06-25 21:04:44 -0400

    docs: Added docstrings for conn methods

--- connections/wikipedia.py
+++ connections/wikipedia.py
@@ -6,6 +6,19 @@ from bs4 import BeautifulSoup
 
 class Wikipedia:
     def search_page(search_term: str, limit: int) -> list:
+        """
+        Hit the search_page Wikipedia endpoint.
+
+        Args:
+            search_term (str): The term to search for
+            limit (int): The number of results to show
+
+        Raises:
+            ConnectionError: If request fails
+
+        Returns:
+            list: List of pages
+        """
         url = WikipediaEnum.URL_SEARCH_PAGE.value
         headers = WikipediaEnum.HEADERS.value
         params = {"q": search_term, "limit": limit}
@@ -17,7 +30,19 @@ class Wikipedia:
             raise ConnectionError("Could not search for wikipedia page")
         return response.json()
 
-    def get_page(title: str):
+    def get_page(title: str) -> BeautifulSoup:
+        """
+        Hit the get_page Wikipedia endpoint.
+
+        Args:
+            title (str): Title of page to search for
+
+        Raises:
+            ConnectionError: If connection fails
+
+        Returns:
+            BeautifuSoup: HTML parser of response
+        """
         url = WikipediaEnum.URL_GET_PAGE.value
         url = url.format(title)
         headers = WikipediaEnum.HEADERS.value
