5850912bddb5dab249c84c41eb67a2ae134050a7
charlotte-alvarez <email@armandalvarez.com>
2026-06-25 21:06:01 -0400

    feat: Command can now show wikipedia page output in human-readable way; has additional option for showing references

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import typer
 from typing import Annotated
 from connections.wikipedia import Wikipedia
+from utils.response_parsing import (
+    parse_wiki_pages_for_title_and_description,
+    parse_wiki_page_for_content,
+    parse_wiki_page_for_references,
+)
 
 app = typer.Typer(help="TAFFY: The Answer Finder for You!")
 
@@ -12,18 +17,13 @@ def search(
         bool, typer.Option(help="Search only wikipedia for page")
     ] = False,
     page_limit: Annotated[int, typer.Option(help="The number of pages to suggest")] = 3,
+    show_references: Annotated[bool, typer.Option(help="Show references")] = False,
 ):
     response = Wikipedia.search_page(search_term, page_limit)
 
     pages = response.get("pages")
-    page_list = []
-    number_of_pages = len(pages)
-    page_num = 1
-    for page in pages:
-        title = page.get("title")
-        description = page.get("description")
-        page_list.append((page_num, title, description))
-        page_num += 1
+    page_list = parse_wiki_pages_for_title_and_description(pages)
+    number_of_pages = len(page_list)
 
     output_text = ""
     for page in page_list:
@@ -45,13 +45,56 @@ def search(
         print("Quitting")
         return
 
-    r = Wikipedia.get_page(page_list[user_input - 1][1])
+    response = Wikipedia.get_page(page_list[user_input - 1][1])
+    content = parse_wiki_page_for_content(response)
+
     if wikipedia:
         print("Only searching in wikipedia")
     else:
         print("Searching all sources")
 
-    print(r)
+    print("\n\n---------------\n\n")
+
+    # Wikipedia
+
+    print(content.pop("Description"))
+
+    while True:
+        print("\n\n")
+
+        if show_references:
+            index = 0
+            print("References:\n")
+            for reference in parse_wiki_page_for_references(response):
+                print(f"{index + 1} - {reference}")
+                index += 1
+
+        output_text = "Additional Sections\n"
+        index = 0
+        for section in content:
+            output_text += f"{index + 1} - {list(content.keys())[index]}\n"
+            index += 1
+        output_text = (
+            f"---\nSee additional section? (1-{len(content)}; 0 quits)\n\n{output_text}"
+        )
+
+        user_input = -1
+        number_of_extra_sections = len(content)
+        while True:
+            user_input = input(output_text)
+            try:
+                user_input = int(user_input)
+                if 0 <= user_input < number_of_extra_sections + 1:
+                    break
+            except TypeError, ValueError:
+                pass
+
+        if user_input == 0:
+            print("Quitting")
+            return
+        else:
+            key = list(content.keys())[user_input - 1]
+            print(content[key])
 
 
 if __name__ == "__main__":
