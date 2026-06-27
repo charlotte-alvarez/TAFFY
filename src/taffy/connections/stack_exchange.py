4a567677d3685756c6d56941b42ac7ede8108aa0
Armand Alvarez <aralvarez159@gmail.com>
2026-06-27 00:57:31 -0400

    Added stack exchange connection methods and some enums

---
+++ src/taffy/connections/stack_exchange.py
@@ -0,0 +1,57 @@
+import requests
+from requests.exceptions import HTTPError
+from taffy.utils.enums import StackExchangeEnum
+from taffy.utils.errors import ConnectionError
+from bs4 import BeautifulSoup
+
+
+class StackExchange:
+    def sites_list() -> list:
+        """
+        Hit the sites endpoint for stack exchange
+
+        Returns:
+            list: List of pages
+
+        Raises:
+            ConnectionError: If request fails
+
+        """
+        url = StackExchangeEnum.URL_SITES.value
+
+        try:
+            response = requests.get(url=url)
+            response.raise_for_status()
+        except HTTPError:
+            raise ConnectionError("Could not search for stack exchange sites")
+        return response.json().get("items")
+
+    def advanced_search(query: str) -> dict:
+        """
+        Hit the advanced search endpoint of the wikipedia api
+
+        Args:
+            query (str): Phrase to search for
+
+        Returns:
+            dict: Dictionary containing items found
+        """
+
+        url = StackExchangeEnum.URL_ADVANCED_SEARCH.value.format(
+            VERSION=StackExchangeEnum.VERSION.value, QUERY=query
+        )
+
+        try:
+            response = requests.get(url=url)
+            response.raise_for_status()
+        except HTTPError:
+            raise ConnectionError("Could not search for query")
+
+        return response.json().get("items")
+
+
+if __name__ == "__main__":
+    l = StackExchange.advanced_search("test")
+    for x in l:
+        print(x)
+        print("\n\n----\n\n")
--- src/taffy/utils/enums.py
+++ src/taffy/utils/enums.py
@@ -7,3 +7,9 @@ class WikipediaEnum(Enum):
     }
     URL_SEARCH_PAGE = "https://en.wikipedia.org/w/rest.php/v1/search/page"
     URL_GET_PAGE = "https://en.wikipedia.org/w/rest.php/v1/page/{}/html"
+
+
+class StackExchangeEnum(Enum):
+    VERSION = "2.3"
+    URL_SITES = f"https://api.stackexchange.com/{VERSION}/sites"
+    URL_ADVANCED_SEARCH = "https://api.stackexchange.com/{VERSION}/search/advanced?order=desc&sort=relevance&q={QUERY}&site=stackoverflow"
