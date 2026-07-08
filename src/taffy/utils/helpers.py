d12456f9a8b120c6d28b25a85b82859fef85b0f8
charlotte-alvarez <email@armandalvarez.com>
2026-07-08 16:59:41 -0400

    fix(utils): fixed user input helper to not throw error if input outside of provided range is selected

--- src/taffy/utils/helpers.py
+++ src/taffy/utils/helpers.py
@@ -10,10 +10,11 @@ def handle_input(output_text: str, number_of_options: int) -> int:
         int: The users selection
     """
 
-    user_input = input(output_text)
-    try:
-        user_input = int(user_input)
-        if 0 <= user_input < number_of_options + 1:
-            return user_input
-    except TypeError, ValueError:
-        pass
+    while True:
+        user_input = input(output_text)
+        try:
+            user_input = int(user_input)
+            if 0 <= user_input < number_of_options + 1:
+                return user_input
+        except TypeError, ValueError:
+            pass
